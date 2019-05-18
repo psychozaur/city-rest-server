6857b9f1ca8bdd487acd283c90d9cdab508960d4
Marcin Rybicki <m.rybicki1112@gmail.com>
2019-05-18 09:32:40 +0200

    [add] - gitignore, change to HELP.md

---
+++ src/main/java/com/rybickim/cityrestserver/CityRestServerApplication.java
@@ -0,0 +1,13 @@
+package com.rybickim.cityrestserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityRestServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityRestServerApplication.class, args);
+    }
+
+}
