528cce5188f08cf21405f43a3bad539d628f6413
Marcin Rybicki <m.rybicki1112@gmail.com>
2019-05-26 14:16:02 +0200

    [refactor] - Hibernate entity and H2 properties and dependency set

--- src/main/java/com/rybickim/cityrestserver/domain/CityEntity.java
+++ src/main/java/com/rybickim/cityrestserver/domain/CityEntity.java
@@ -3,12 +3,23 @@ package com.rybickim.cityrestserver.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 @Data
-@AllArgsConstructor
 public class CityEntity {
-    private int repoId;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String cityName;
     private int population;
+
+    public CityEntity() {
+    }
 
     public CityEntity(String cityName, int population) {
         this.cityName = cityName;
